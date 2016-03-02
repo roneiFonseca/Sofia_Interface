d5d505bcaff0ea97f85932c50794c8d57fb945b6
PedroPenaforte <pedropenaforte@hotmail.com>
2016-03-02 18:01:22 -0300

    Funcionalidades dos botoes adicionadas, tempo pode variar de 4 a 12 min.

--- PyQt/time_window.py
+++ PyQt/time_window.py
@@ -8,6 +8,8 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
+import imagens
+import parametros
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -26,7 +28,7 @@ except AttributeError:
 class Ui_fourthDialog(object):
     def setupUi(self, fourthDialog):
         fourthDialog.setObjectName(_fromUtf8("fourthDialog"))
-        fourthDialog.resize(800, 465)
+        fourthDialog.resize(800, 480)
         fourthDialog.setStyleSheet(_fromUtf8("background-color: rgb(255, 255, 255);"))
         self.label_2 = QtGui.QLabel(fourthDialog)
         self.label_2.setGeometry(QtCore.QRect(220, 130, 341, 71))
@@ -41,7 +43,7 @@ class Ui_fourthDialog(object):
         self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.pushButton_5 = QtGui.QPushButton(fourthDialog)
-        self.pushButton_5.setGeometry(QtCore.QRect(450, 390, 141, 51))
+        self.pushButton_5.setGeometry(QtCore.QRect(310, 390, 141, 51))
         self.pushButton_5.setStyleSheet(_fromUtf8("font: 14pt \"Arial\";"))
         self.pushButton_5.setObjectName(_fromUtf8("pushButton_5"))
         self.label = QtGui.QLabel(fourthDialog)
@@ -137,6 +139,7 @@ class Ui_fourthDialog(object):
         self.lcdNumber_2.setStyleSheet(_fromUtf8("background-color: rgb(0, 0, 0);\n"
 "selection-background-color: rgb(0, 0, 127);"))
         self.lcdNumber_2.setObjectName(_fromUtf8("lcdNumber_2"))
+        self.lcdNumber_2.display(parametros.todos['tempo'])
         self.pushButton_3 = QtGui.QPushButton(fourthDialog)
         self.pushButton_3.setGeometry(QtCore.QRect(590, 250, 81, 81))
         font = QtGui.QFont()
@@ -153,10 +156,7 @@ class Ui_fourthDialog(object):
         self.label_12.setGeometry(QtCore.QRect(480, 310, 61, 31))
         self.label_12.setStyleSheet(_fromUtf8("font: 75 14pt \"Arial\";"))
         self.label_12.setObjectName(_fromUtf8("label_12"))
-        self.pushButton = QtGui.QPushButton(fourthDialog)
-        self.pushButton.setGeometry(QtCore.QRect(200, 390, 141, 51))
-        self.pushButton.setStyleSheet(_fromUtf8("font: 14pt \"Arial\";"))
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+
 
         self.retranslateUi(fourthDialog)
         QtCore.QObject.connect(self.pushButton_5, QtCore.SIGNAL(_fromUtf8("clicked()")), fourthDialog.close)
@@ -169,9 +169,27 @@ class Ui_fourthDialog(object):
         self.pushButton_3.setText(_translate("fourthDialog", "+", None))
         self.pushButton_4.setText(_translate("fourthDialog", "-", None))
         self.label_12.setText(_translate("fourthDialog", "min", None))
-        self.pushButton.setText(_translate("fourthDialog", "VOLTAR", None))
+        QtCore.QObject.connect(self.pushButton_3 , QtCore.SIGNAL("clicked()") , self.time_button_Plus_click)
+        QtCore.QObject.connect(self.pushButton_4 , QtCore.SIGNAL("clicked()") , self.time_button_Minus_click)
 
-import imagens
+    def time_button_Plus_click(self):
+        parametros.todos['tempo'] +=1
+        self.lcdNumber_2.display(parametros.todos['tempo'])
+        while parametros.todos['tempo'] > 12:
+            self.lcdNumber_2.display(12)
+            parametros.todos['tempo'] = 12      
+
+    def time_button_Minus_click(self):
+        parametros.todos['tempo'] -=1
+        self.lcdNumber_2.display(parametros.todos['tempo'])
+        while parametros.todos['tempo'] < 4:
+            self.lcdNumber_2.display(4)
+            parametros.todos['tempo'] = 4   
+    
+
+           
+
+
 
 if __name__ == "__main__":
     import sys
