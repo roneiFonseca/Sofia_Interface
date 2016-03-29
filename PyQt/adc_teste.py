c0a5de1f340cc5dca57dcc70626fb7ad58360786
Pedro-M-Santos <unbmatiaspedro@gmail.com>
2016-03-29 16:12:01 -0300

    Mudanças no teste do ADC
    Adicionamos 2 PCF8591. Um funcionará como DAC e o outro como ADC

--- PyQt/adc_teste.py
+++ PyQt/adc_teste.py
@@ -1,22 +1,28 @@
+# coding=utf-8
+
 from __future__ import division
 import smbus
 import time
 import os
 
 bus = smbus.SMBus(1)
+address2 = 0x48
+address1 = 0x4c
+# voltageValue =128
 
-try:
+while True:
 
-   while True:
+	bus.write_byte(address1, 0) #Requisitando leitura do Canal 0 do PCF8591 (1)
+	voltageValue1 = bus.read_byte(address2) #Realizando leitura do Canal 0 do PCF8591 (1)
+	time.sleep(0.5)
+	bus.write_byte(address2, 0) #Requisitando leitura do Canal 0 do PCF8591 (1)
+	voltageValue2 = bus.read_byte(address1) #Realizando leitura do Canal 0 do PCF8591 (1)
+	# time.sleep(0.5)
+	print "Tensão (0x48): " + str(voltageValue1)
+	print "Tensão (0x4c): " + str(voltageValue2)
+	# bus.write_byte_data(address1, 0x44,voltageValue)
 
-         bus.write_byte(0x48, 0)
-         x = bus.read_byte(0x48)
-         y = (3/5)*x-73
-         print('Temperatura: ' + str(y))
-         time.sleep(0.5)
-	 os.system('clear')
-except:
-   pass
+
 
 
 
