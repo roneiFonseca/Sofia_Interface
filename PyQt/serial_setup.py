5224bad713900f1716a9c05766e68517f0236874
roneiFonseca <ronei.del.fonseca@gmail.com>
2016-04-13 10:39:01 -0300

    Funçao de conexao com o VERA

--- PyQt/serial_setup.py
+++ PyQt/serial_setup.py
@@ -6,22 +6,23 @@ from serial import SerialException
 
 def serial_setup():
   conected_flag = False
-
-  try:
-    print 'Tentnado conexcao com VERA...'
-    ser = serial.Serial(
-    port= '/dev/ttyAMA0',
-    baudrate = 9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
+  for x in xrange(0,10):
+    try:
+      print 'Tentnado conexcao com VERA...'
+      ser = serial.Serial(
+      port= '/dev/ttyAMA0',
+      baudrate = 9600,
+      parity=serial.PARITY_NONE,
+      stopbits=serial.STOPBITS_ONE,
+      bytesize=serial.EIGHTBITS,
+      timeout=1
+      break
   )
 
-  except Exception as e:
-    # raise e
-    print "ERRO NA CONEXAO SERIAL I/O ERRO({0}): {1}".format(e.errno, e.strerror)
-    return conected_flag
+    except Exception as e:
+      # raise e
+      print "ERRO NA CONEXAO SERIAL I/O ERRO({0}): {1}".format(e.errno, e.strerror)
+      return conected_flag
   
   print 'Conexao estabelicida.'
   conected_flag = True
