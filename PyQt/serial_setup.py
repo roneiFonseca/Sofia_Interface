742d6d917c68e4029af2d4e91490dd0ea69b13f6
PedroPenaforte <pedropenaforte@hotmail.com>
2016-04-16 01:10:37 -0300

    Problemas de identaçao corrigidos

--- PyQt/serial_setup.py
+++ PyQt/serial_setup.py
@@ -8,22 +8,21 @@ def serial_setup():
   conected_flag = False
   for x in xrange(0,10):
     try:
-      print 'Tentnado conexcao com VERA...'
+      print 'Tentando conexao com VERA...'
       ser = serial.Serial(
       port= '/dev/ttyAMA0',
       baudrate = 9600,
       parity=serial.PARITY_NONE,
       stopbits=serial.STOPBITS_ONE,
       bytesize=serial.EIGHTBITS,
-      timeout=1
+      timeout=1)
       break
-  )
 
     except Exception as e:
       # raise e
       print "ERRO NA CONEXAO SERIAL I/O ERRO({0}): {1}".format(e.errno, e.strerror)
       return conected_flag
   
-  print 'Conexao estabelicida.'
+  print 'Conexao estabelecida.'
   conected_flag = True
   return conected_flag
